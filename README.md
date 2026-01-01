efef9548edd258a7f4a8dcaa53192a902bc874cb
Dmytro Krasun <krasun.net@gmail.com>
2026-01-01 22:53:49 +0200

    Initialize the project

---
+++ README.md
@@ -0,0 +1,9 @@
+# How Browsers Work
+
+## Development
+
+Run:
+
+```bash
+npm run dev
+```
---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "How Browsers Work",
+    description: "A guide on how browsers work.",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
